f03b3cdc3c182c424b852c30181acce6305d3f53
bmargerison <bmargerison@outlook.com>
2021-11-24 16:09:43 +0000

    decrease temp

--- thermostat.js
+++ thermostat.js
@@ -1,15 +1,30 @@
+const min_temp = 10;
+
 class Thermostat {
-  contructor() {
+
+  constructor() {
     this.temperature = 20;
   }
 
-  get getTemperature() {
-    return this.temperature;
+  up() {
+    this.temperature++;
   }
 
-  up() {
-    this.temperature += 1
+  down() {
+    if (this.temeprature <= Thermostat.min_temp) {
+       this.temperature = Thermostat.min_temp;
+    } else {
+      this.temperature -= 1;
+    }
+   // return(this.temeprature <= Thermostat.min_temp ? this.temperature = Thermostat.min_temp : this.temperature--);
+  }
+
+  static get min_temp() {
+    return min_temp
   }
 }
+
+
+
 
 module.exports = Thermostat;
--- thermostat.test.js
+++ thermostat.test.js
@@ -3,12 +3,26 @@ const Thermostat = require('./thermostat.js');
 describe('thermostat', () => {
   test('starts at 20 degrees', () => {
     thermostat = new Thermostat();
-    expect(thermostat.getTemperature).toBe(20);
+    expect(thermostat.temperature).toBe(20);
   });
 
   test('increases temperature by 1 degree', () => {
     thermostat = new Thermostat();
-    thermostat.up
-    expect(thermostat.getTemperature).toBe(21);
+    thermostat.up()
+    expect(thermostat.temperature).toBe(21);
+  });
+
+  test('decreases temperature by 1 degree', () => {
+    thermostat = new Thermostat();
+    thermostat.down()
+    expect(thermostat.temperature).toBe(19);
+  });
+  
+  test('cannot go below minimum 10 degrees', () => {
+    thermostat = new Thermostat();
+    for (i=0; i < 12; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.temperature).toBe(10);
   });
 });
