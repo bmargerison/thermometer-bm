612c83e647f7f3f3d97ee2cadbb2c67360506252
Myoung <mhbae93@gmail.com>
2021-11-24 16:14:34 +0000

    added min temperature catch. tests all passing

--- thermostat.js
+++ thermostat.js
@@ -1,7 +1,6 @@
 const min_temp = 10;
 
 class Thermostat {
-
   constructor() {
     this.temperature = 20;
   }
@@ -11,20 +10,14 @@ class Thermostat {
   }
 
   down() {
-    if (this.temeprature <= Thermostat.min_temp) {
-       this.temperature = Thermostat.min_temp;
-    } else {
-      this.temperature -= 1;
-    }
-   // return(this.temeprature <= Thermostat.min_temp ? this.temperature = Thermostat.min_temp : this.temperature--);
+    return this.temperature <= Thermostat.min_temp
+      ? this.temperature
+      : this.temperature--;
   }
 
   static get min_temp() {
-    return min_temp
+    return min_temp;
   }
 }
-
-
-
 
 module.exports = Thermostat;
--- thermostat.test.js
+++ thermostat.test.js
@@ -1,28 +1,29 @@
-const Thermostat = require('./thermostat.js');
+const Thermostat = require("./thermostat.js");
 
-describe('thermostat', () => {
-  test('starts at 20 degrees', () => {
+describe("thermostat", () => {
+  test("starts at 20 degrees", () => {
     thermostat = new Thermostat();
     expect(thermostat.temperature).toBe(20);
   });
 
-  test('increases temperature by 1 degree', () => {
+  test("increases temperature by 1 degree", () => {
     thermostat = new Thermostat();
-    thermostat.up()
+    thermostat.up();
     expect(thermostat.temperature).toBe(21);
   });
 
-  test('decreases temperature by 1 degree', () => {
+  test("decreases temperature by 1 degree", () => {
     thermostat = new Thermostat();
-    thermostat.down()
+    thermostat.down();
     expect(thermostat.temperature).toBe(19);
   });
-  
-  test('cannot go below minimum 10 degrees', () => {
+
+  test("cannot go below minimum 10 degrees", () => {
     thermostat = new Thermostat();
-    for (i=0; i < 12; i++) {
+    for (i = 0; i < 10; i++) {
       thermostat.down();
     }
+    thermostat.down();
     expect(thermostat.temperature).toBe(10);
   });
 });
